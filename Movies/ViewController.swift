5e23cb4d88ec4ea88870cf88e06a145946bd6467
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-05-20 09:55:18 -0600

    Pantalla de Login terminada y SDK Firebase listo

---
+++ Movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Marco Alonso Rodriguez on 20/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
