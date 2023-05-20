0925ed5148c4911a31780308cfa45822d1f54d93
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-05-20 11:13:24 -0600

    Guardando la sesión con UserDefaults

---
+++ Movies/HomeViewController.swift
@@ -0,0 +1,37 @@
+//
+//  HomeViewController.swift
+//  Movies
+//
+//  Created by Marco Alonso Rodriguez on 20/05/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    ///Una instancia de bd userdefaults
+    let defaults = UserDefaults.standard
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //Guardar sesion en BD
+        defaults.set("logueado", forKey: "sesionIniciada")
+    }
+    
+
+    @IBAction func salirButton(_ sender: UIButton) {
+        //Borrar la sesion 
+        defaults.removeObject(forKey: "sesionIniciada")
+        navegarLogin()
+    }
+    
+    func navegarLogin(){
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "LoginViewController") as! LoginViewController
+        vc.modalPresentationStyle = .fullScreen
+        vc.modalTransitionStyle = .crossDissolve
+        self.present(vc, animated: true)
+    }
+
+}
