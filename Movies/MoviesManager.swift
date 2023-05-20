f630c4c495af9483c4f80b7ccf167f46f9ae9086
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-05-20 11:55:38 -0600

    MovieDataModel agregado

---
+++ Movies/MoviesManager.swift
@@ -0,0 +1,16 @@
+//
+//  MoviesManager.swift
+//  Movies
+//
+//  Created by Marco Alonso Rodriguez on 20/05/23.
+//
+
+import Foundation
+
+struct MoviesManager {
+    
+
+    func getPopularMovies(completion: @escaping ([DataMovie] -> Void)) {
+        
+    }
+}
