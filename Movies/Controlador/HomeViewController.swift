c87ddd338261709216cb520a8366f9b55019f13c
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-05-20 12:30:28 -0600

    Collection Movies terminado

--- Movies/Controlador/HomeViewController.swift
+++ Movies/Controlador/HomeViewController.swift
@@ -10,6 +10,11 @@ import FirebaseAuth
 
 class HomeViewController: UIViewController {
     
+    
+    @IBOutlet weak var moviesCollection: UICollectionView!
+    
+    var peliculas: [DataMovie] = []
+    
     let manager = MoviesManager()
     
     ///Una instancia de bd userdefaults
@@ -18,12 +23,24 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        //Guardar sesion en BD
+        moviesCollection.register(UINib(nibName: "MovieCell", bundle: nil), forCellWithReuseIdentifier: "celda")
+        
+        moviesCollection.delegate = self
+        moviesCollection.dataSource = self
+        
         defaults.set("logueado", forKey: "sesionIniciada")
         
         manager.getPopularMovies { listadoPeliculas in
             print("num pelis: \(listadoPeliculas.count)")
         }
+    }
+    
+    func navegarLogin(){
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "LoginViewController") as! LoginViewController
+        vc.modalPresentationStyle = .fullScreen
+        vc.modalTransitionStyle = .crossDissolve
+        self.present(vc, animated: true)
     }
     
 
@@ -39,13 +56,24 @@ class HomeViewController: UIViewController {
             print("Debug: error al cerrar sesion \(error.localizedDescription)")
         }
     }
-    
-    func navegarLogin(){
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let vc = storyboard.instantiateViewController(withIdentifier: "LoginViewController") as! LoginViewController
-        vc.modalPresentationStyle = .fullScreen
-        vc.modalTransitionStyle = .crossDissolve
-        self.present(vc, animated: true)
-    }
+}
 
+// MARK:  CollectionView
+extension HomeViewController : UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return peliculas.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let celda = collectionView.dequeueReusableCell(withReuseIdentifier: "celda", for: indexPath) as! MovieCell
+        
+        celda.posterMovie.image = UIImage(systemName: "note")
+        celda.titleMovie.text = "Mario Bros"
+        celda.dateMovie.text = "Mayo 2023"
+        celda.overviewMovie.text = "Descripcion pelicula"
+        
+        return celda
+    }
+    
+    
 }
--- Movies/Vista/MovieCell.swift
+++ Movies/Vista/MovieCell.swift
@@ -8,6 +8,13 @@
 import UIKit
 
 class MovieCell: UICollectionViewCell {
+    
+    @IBOutlet weak var titleMovie: UILabel!
+    
+    @IBOutlet weak var overviewMovie: UITextView!
+    @IBOutlet weak var dateMovie: UILabel!
+    @IBOutlet weak var posterMovie: UIImageView!
+    
 
     override func awakeFromNib() {
         super.awakeFromNib()
